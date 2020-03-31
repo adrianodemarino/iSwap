d16fdc6b4675f7b6dfba06d7e1c31a07a8f7f6ba
adrianodemarino <adriano.demarino@gmail.com>
2020-03-31 02:03:53 +0200

    new picture

---
+++ barcode_pictures.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+import pandas as pd
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.colors as colors
+import seaborn as sns
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', dest="file", help="", action="store", required=True)
+
+args = parser.parse_args()
+file = args.file
+
+import re
+def humanSorted(l):
+    def tryint(s):
+        try:
+            return int(s)
+        except:
+            return s
+    def alphanum_key(s):
+        return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+    def sort_nicely(l):
+        return sorted(l, key=alphanum_key)
+    return sort_nicely(l)
+
+df = pd.read_csv(file,sep='\t').drop_duplicates()
+
+#First Picture#################################################################
+a = pd.pivot_table(df,values="expected",columns="bc2",index="bc1").fillna(0)
+a = a.reindex(index=humanSorted(df.bc1.unique()), columns=humanSorted(df.bc2.unique()))
+f, ax = plt.subplots(figsize=(18, 12))
+
+colors = ["gray", "cyan", "green"]
+cmap = LinearSegmentedColormap.from_list('Custom', colors, len(colors))
+
+# Draw the heatmap with the mask and correct aspect ratio
+_ = sns.heatmap(a, cmap=cmap,square=True,  linewidths=.5, cbar_kws={"shrink": .5})
+
+# Set the colorbar labels
+colorbar = ax.collections[0].colorbar
+colorbar.set_ticks([0.35,0.95,1.65])
+colorbar.set_ticklabels(['Unused combination', 'Expected combination',"Swapped fraction"])
+ax.invert_xaxis()
+
+ax.set_xlabel("Barcode 2",fontsize=20)
+ax.set_ylabel("Barcode 1",fontsize=20)
+ax.tick_params(labelsize=11)
+
+ax.get_figure().savefig(file.split('.')[0]+'_barcodeStrategy.png',format='png', dpi=500)
+##########################################################################################
+
+
+#Second Picture#################################################################
+b = pd.pivot_table(df,values="reads",columns="bc2",index="bc1").fillna(0)
+b = b.reindex(index=humanSorted(df.bc1.unique()), columns=humanSorted(df.bc2.unique()))
+fig, bx = plt.subplots(figsize=(10, 10))
+
+log_norm = LogNorm()
+# Draw the heatmap with the mask and correct aspect ratio
+# color bar keyword arguments
+cbar_ticks = [math.pow(10, i) for i in range(math.floor(math.log10(df.reads.min())), 1+math.ceil(math.log10(df.reads.max())))]
+cbar_kws = {"shrink":.5,'label':'Log10 read counts',"ticks": cbar_ticks}
+
+pcm = sns.heatmap(b, cmap="viridis",square=True,  linewidths=.5, cbar_kws=cbar_kws,norm=log_norm)
+
+bx.invert_xaxis()
+bx.set_xlabel("Barcode 2",fontsize=20)
+bx.set_ylabel("Barcode 1",fontsize=20)
+bx.tick_params(labelsize=11)
+
+bx.get_figure().savefig(file.split('.')[0]+'_LogReadsCount.png',format='png', dpi=500)
+
